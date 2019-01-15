3892066001710d1d1a1776e35bfab2fb1fd37ef9
Igor Babic <44980330+ibabic22@users.noreply.github.com>
2019-01-15 16:41:56 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -18,6 +18,24 @@ $(document).ready(function() {
 $('#addRow').click();
 
 
+$(document).ready(function() {
+  var table = $('#myTable').DataTable();
+
+  $('#myTable tbody').on( 'click', 'tr', function () {
+      if ( $(this).hasClass('selected') ) {
+          $(this).removeClass('selected');
+      }
+      else {
+          table.$('tr.selected').removeClass('selected');
+          $(this).addClass('selected');
+      }
+  } );
+
+  $('#deleteRow').click( function () {
+      table.row('.selected').remove().draw( false );
+  } );
+} );
+
 /**
  * ---------------------------------------
  * This demo was created using amCharts 4.
