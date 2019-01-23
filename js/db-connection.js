cfb60b3db0579ad7a2ff7134e5a74db1edcad84d
Igor Babic <44980330+ibabic22@users.noreply.github.com>
2019-01-23 16:36:06 +0100

    Add files via upload

---
+++ js/db-connection.js
@@ -0,0 +1,19 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "sqldemo.softmetrixgroup.com",
+  user: "root",
+  password: "smx1111",
+  database: 'moneyTracker'
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+
+    var sql = "INSERT INTO main (main_id, 'main_date', main_cat, main_sum, main_currency, main_com) VALUES ('1', '2019-01-23',shopping', '300' , 'RSD', 'kupilo')";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("1 record inserted");
+    });
+});
