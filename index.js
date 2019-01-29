12cce2187aaee73699f3aed63b1d8e93662079dc
Igor Babic <44980330+ibabic22@users.noreply.github.com>
2019-01-29 20:26:03 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const path = require('path')
+const http = require('http');
+const mysql = require('mysql');
+const port = 3000;
+const bodyparser = require('body-parser');
+
+const con = mysql.createConnection({
+    host: 'sqldemo.softmetrixgroup.com',
+    port: '3306',
+    user: 'root',
+    password: 'smx1111',
+    database: 'moneyTracker',
+    multipleStatements: true
+});
+
+// Body Parser Middleware
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+
+// View Engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Set Static Path
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('js', express.static('js'))
+
+app.get('/', function(req, res) {
+    con.query('SELECT main_id, main_date, main_cat, main_sum, main_currency, main_com FROM main', function(err, result) {
+        if (err) {
+            throw err;
+        } else {
+            main = result;
+            //console.log(main);
+            res.render('index', main);
+        }
+    })
+})
+
+app.listen(port, function() {
+    console.log(`Example app listening on port ${port}!`);
+});
+
--- js/db-connection.js
+++ js/db-connection.js
@@ -1,27 +1,27 @@
-const mysql = require('mysql');
+// const mysql = require('mysql');
 
-var con = mysql.createConnection({
-  host: "sqldemo.softmetrixgroup.com",
-  user: "root",
-  password: "smx1111",
-  database: 'moneyTracker'
-});
+// var con = mysql.createConnection({
+//   host: "sqldemo.softmetrixgroup.com",
+//   user: "root",
+//   password: "smx1111",
+//   database: 'moneyTracker'
+// });
+
+// // con.connect(function(err) {
+// //     if (err) throw err;
+// //     console.log("Connected!");
+
+// //     var sql = "INSERT INTO main (main_id, main_date, main_cat, main_sum, main_currency, main_com) VALUES ('1', '2016-12-23',shopping', '300' , 'RSD', 'kupilo')";
+// //     con.query(sql, function (err, result) {
+// //         if (err) throw err;
+// //         console.log("1 record inserted");
+// //     });
+// // });
 
 // con.connect(function(err) {
 //     if (err) throw err;
-//     console.log("Connected!");
-
-//     var sql = "INSERT INTO main (main_id, 'main_date', main_cat, main_sum, main_currency, main_com) VALUES ('1', '',shopping', '300' , 'RSD', 'kupilo')";
-//     con.query(sql, function (err, result) {
-//         if (err) throw err;
-//         console.log("1 record inserted");
+//     con.query("SELECT * FROM main", function (err, result) {
+//       if (err) throw err;
+//       console.log(result);
 //     });
-// });
-
-con.connect(function(err) {
-    if (err) throw err;
-    con.query("SELECT * FROM main", function (err, result, fields) {
-      if (err) throw err;
-      console.log(result);
-    });
-  });
+//   });
