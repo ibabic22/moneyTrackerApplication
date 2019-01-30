6e2c68737780a9c1f4febe1d15b01b63da163469
Igor Babic <44980330+ibabic22@users.noreply.github.com>
2019-01-30 16:44:03 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -28,8 +28,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('js', express.static('js'))
 
+app.post('/moneytracker/main', function(req, res) {
+    let newExpense = {
+        main_date: req.body.main_date,
+        main_cat: req.body.main_cat,
+        main_sum: req.body.main_sum,
+        main_com: req.body.main_com
+    }
+    let query = "INSERT INTO `main` (main_id ,main_date, main_cat, main_sum, main_com)";
+});
+
+
 app.get('/', function(req, res) {
-    con.query('SELECT main_id, main_date, main_cat, main_sum, main_currency, main_com FROM main', function(err, result) {
+    con.query('SELECT main_id, main_date, main_cat, main_sum, main_com FROM main', function(err, result) {
         if (err) {
             throw err;
         } else {
@@ -39,6 +50,7 @@ app.get('/', function(req, res) {
         }
     })
 })
+
 
 app.listen(port, function() {
     console.log(`Example app listening on port ${port}!`);
